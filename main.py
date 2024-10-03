cb1bae14d5751b619c04f4d8fdc1152f00fcb132
vieirinha <89489966+vieirinha1104@users.noreply.github.com>
2024-10-03 15:55:01 -0300

    Project's Main Code, now working and cleaner

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import itertools
 import gurobipy as gp
 from gurobipy import Model, GRB
 
-# Variables
-n = 3  # Number of qubits
-d = 2  # Maximum number of gates of the circuit
+# Input Variables
+n, d = (3, 2)  # N: Number of qubits, d: Maximum number of gates of the circuit
 
-# Initialize arrays
+# Aux vars
 arr = [0] * n  # Aux array for storing qubit states
 brr = ['-'] * n  # Aux array for storring Toffoli gates
 
@@ -23,23 +22,21 @@ costs = [
 ]
 
 # Arrays for the Omega Partition
-omega_input = []
-omega_output = []
+omega_input, omega_output = ([], [])
 
 # Dictonary for indexing the Toffoli gates into their costs
-costs_dict = {}
-# Example: 0: ['+', 'o', 'o'] -> costs_dict[0] =  5 
+costs_dict = {} # Example: 0: ['+', 'o', 'o'] -> costs_dict[0] =  5 
 
-# Dictionaries for indexing qubit states and Toffoli gates
-qubits_dict = {}  # Dictionary for all possible qubit arrays
-# Example: {0: [0, 0, 0], 1: [0, 0, 1], 2: [0, 1, 0], 3: [0, 1, 1], ... }
+# Dictionaries for indexing qubit states 
+qubits_dict = {} # Example: {0: [0, 0, 0], 1: [0, 0, 1], 2: [0, 1, 0], 3: [0, 1, 1], ... }
 
-toffoli_dict = {}  # Dictionary for indexing all possible Toffoli gates
-# 'o': control qubit, '+': target qubit, '-': slack qubit
+# Dictionary for indexing all possible Toffoli gates
+toffoli_dict = {} # 'o': control qubit, '+': target qubit, '-': slack qubit
 
-flipQ_dict = {} # flipQ_dict[key] = value, value is the position of the flipped qubit by arc key
+# Dictionary for finding the position of the flipped qubit by arc key
+flipQ_dict = {} 
+
 # Functions
-
 # This function set all the elements in the toffoli_dict, O(n*(3^n)) 
 def setToffoliDict(k):
     if(k == n):
@@ -75,7 +72,6 @@ def setQubitsDict(k):
             setQubitsDict(k+1)
             arr[k] = aux
 
-
 # This function returns the qubits output of f(qi), where qi: qubits input and f: toffoli gate, O(n)
 def transition(index_q, index_f):
     #index_q is the index of q in qubits_dict
@@ -107,7 +103,7 @@ def multilayered_graph(n,d):
     G = nx.DiGraph()
     for i in range(1, d+2):
         for j in range(0, 2**n):
-            node = (j,i)
+            node = (j, i)
             G.add_node(node, layer = i)
     for i in range(1, d+1):
         nodes_per_layer = [node for node, data in G.nodes(data=True) if data.get('layer') == i]
@@ -121,12 +117,11 @@ def multilayered_graph(n,d):
                 else:
                     G.add_edge(node, vertex, weight = costs_dict[gate], layer = i, flip = True, keep = False)
                     flipQ_dict[(node, vertex)] = new_state[1]
-
     return G
 
 def omegaPartition():
     boolean_function = {}
-    file_name = 'ex2.txt'
+    file_name = 'ex2.txt' # ex1: paper's example 1 instance, regular instance (d=3), ex2: paper's example 2 instance, dont care instance (d=2)
     data = []
     with open(file_name, 'r') as file:
         for row in file:
@@ -156,7 +151,6 @@ def omegaPartition():
                 aux.append(j)
                 check[j] = True
         omega_input.append(aux)
-
     for x in omega_input:
         output = boolean_function[x[0]]
         new_output = []
@@ -174,23 +168,20 @@ def plotGraph(G):
     pos = {}
     layer_spacing = 2  # Vertical spacing between layers
     node_spacing = 1.5  # Horizontal spacing between nodes
-
     # Calculate pos for plot
     for layer in range(0,d+3):
         nodes_in_layer = [node for node in G.nodes() if node[1] == layer]
         nodes_in_layer.sort(key=lambda x: x[0])  # Sort nodes by their index in qubits_dict
         for idx, node in enumerate(nodes_in_layer):
             pos[node] = (idx * node_spacing, -layer * layer_spacing)
-
     # Plot the Graph
     plt.figure(figsize=(12, 8))
     nx.draw(G, pos, with_labels=True, node_size=700, node_color='lightblue', arrows=True, arrowstyle='-|>', arrowsize=20)
-
     plt.axis("equal")
     plt.title('Multipartite Graph')
     plt.show()
 
-# Prints:
+# Prints for debug:
 def printEverything(G):
     print("Nodes: ",G.nodes())
     print("\n")
@@ -216,8 +207,9 @@ def find_zero_bit_positions(number, N):
 
 # Print the Circuit
 def printSolution(m, n, d, t, w):
-    if m.status == GRB.OPTIMAL:
+    if (m.status == GRB.OPTIMAL):
         print("Circuit Cost: ", m.ObjVal)
+        print("Circuit Design:\n")
         for j in range (1,d+1):
             print("(", end = '')
             for i in range (1,n+1):
@@ -229,10 +221,48 @@ def printSolution(m, n, d, t, w):
                     print("-", end = '')
                 if(i != n):
                     print(", ", end = '')
-            print(")", end = ' ')
+            print(")", end = '')
             print("\n")
     else:
-        print("No solution")
+        print("Model is infeasible.")
+        m.computeIIS()
+        print("IIS computed. Constraints that are conflicting:")
+        for c in m.getConstrs():
+            if c.IISConstr:
+                print(c.constrName)
+
+# Build the graph G_k for a given commodity k
+def set_Gk(G, D, k):
+    H = G.copy()
+    H.add_node(('s', 0), layer = 0)
+    H.add_node(('t', D+2), layer = D+2)
+    # Source Arcs:
+    for v in omega_input[k-1]:
+        H.add_edge(('s', 0), (v, 1), weight = 0, layer = 0, keep = False, flip = False)
+    for u in omega_output[k-1]:
+        H.add_edge((u, D+1), ('t', D+2), weight = 0, layer = D+1, keep = False, flip = False)
+    return H
+
+# k_arcs is the set of all (e,k), where e is an arc of G_k 
+def set_Karcs(G, D, k):
+    k_arcs = []
+    for i in range(1, k+1):
+        H = set_Gk(G, D, i)
+        for e in H.edges():
+            k_arcs.append((e, i))
+        # plotGraph(H)
+    return k_arcs
+
+# Return the set of flip arcs and keep arcs for a given G_k
+def set_keep_and_flip_arcs(H):
+    keep_arcs, flip_arcs = ([],[])
+    for e in H.edges():
+        u, v = (e[0], e[1])
+        if(H[u][v]["keep"] == True):
+            keep_arcs.append(e)
+        if(H[u][v]["flip"] == True):
+            flip_arcs.append(e)
+    return flip_arcs, keep_arcs
 
 # Gurobi Code
 def flowModel(G, N, D, f):
@@ -244,38 +274,20 @@ def flowModel(G, N, D, f):
     w = m.addVars(N+1, D+1, vtype = GRB.BINARY, name ="w")
     # Objective Function
     m.setObjective(gp.quicksum(gp.quicksum(f[N-j][j-1]*y[j, d] for j in range(1, N+1)) for d in range(1, D+1)), GRB.MINIMIZE) # objective function 1a, f[N-j][j-1] := N-j slack qubits and j-1 control qubit
-    # Constraints
+    # Constraints 1b-1f
     m.addConstrs(t[q, d] + w[q, d] <= 1 for q in range(1, N+1) for d in range(1, D+1)) # constraints 1b
     m.addConstrs(gp.quicksum(t[q, d] for q in range(1, N+1)) <= 1 for d in range(1, D+1)) # constraint 1c
     m.addConstrs(w[q, d] <= gp.quicksum(t[r, d] for r in range(1,N+1)) for q in range(1, N+1) for d in range(1, D+1)) # constraints 1d
     m.addConstrs(gp.quicksum(j*y[j, d] for j in range(1, N+1)) == (gp.quicksum(t[q, d] for q in range(1, N+1)) + gp.quicksum(w[q, d] for q in range(1, N+1))) for d in range(1, D+1)) # constraint 1e
     m.addConstrs(gp.quicksum(y[j, d] for j in range(1, N+1)) <= 1 for d in range(1, D+1)) # constraint 1f
-    k = len(omega_input) # commodities
-    k_arcs = []
-    for i in range(1, k+1):
-        H = G.copy()
-        H.add_node(('s', 0), layer = 0)
-        H.add_node(('t', D+2), layer = D+2)
-        # Source Arcs:
-        for v in omega_input[i-1]:
-            H.add_edge(('s', 0), (v, 1), weight = 0, layer = 0, keep = False, flip = False)
-        for u in omega_output[i-1]:
-            H.add_edge((u, D+1), ('t', D+2), weight = 0, layer = D+1, keep = False, flip = False)
-        for e in H.edges():
-            k_arcs.append((e, i))
-        # plotGraph(H)
+    # Commodities
+    k = len(omega_input) 
+    k_arcs = set_Karcs(G, D, k)
     # Flow Decision Vars
-    x = m.addVars(k_arcs, vtype = GRB.CONTINUOUS, lb = 0, ub = 1, name = "x")
+    x = m.addVars(k_arcs, vtype = GRB.BINARY, name = "x")
     # Flow Constraints
     for i in range(1, k+1):
-        H = G.copy()
-        H.add_node(('s', 0), layer = 0)
-        H.add_node(('t', D+2), layer = D+2)
-        # Source Arcs:
-        for v in omega_input[i-1]:
-            H.add_edge(('s', 0), (v, 1), weight = 0, layer = 0, keep = False, flip = False)
-        for u in omega_output[i-1]:
-            H.add_edge((u, D+1), ('t', D+2), weight = 0, layer = D+1, keep = False, flip = False)
+        H = set_Gk(G, D, i)
         # Constraints 1g
         for v in H.nodes():
             a_in = H.in_edges(v) # sigma_{-}(v)
@@ -287,33 +299,15 @@ def flowModel(G, N, D, f):
             else:
                 m.addConstr(gp.quicksum(x[a, i] for a in a_out) - gp.quicksum(x[a, i] for a in a_in) == 0)
         # Constraints 2a, 2b, 2c
-        keep_arcs = []
-        flip_arcs = []
-        for e in H.edges():
-            u = e[0]
-            v = e[1]
-            if(H[u][v]["keep"] == True):
-                keep_arcs.append(e)
-            if(H[u][v]["flip"] == True):
-                flip_arcs.append(e)
-        for j in range(0,len(flip_arcs)):
-            a = flip_arcs[j]
-            b = flipQ_dict[flip_arcs[j]]
-            c = flip_arcs[j][0][1]
+        flip_arcs, keep_arcs = set_keep_and_flip_arcs(H)
+        a, b = (len(flip_arcs), len(keep_arcs))
+        for j in range(0, a):
             m.addConstr(x[flip_arcs[j], i] <= t[flipQ_dict[flip_arcs[j]], flip_arcs[j][0][1]]) # 2a
             m.addConstrs(x[flip_arcs[j], i] <= (1 - w[p, flip_arcs[j][0][1]]) for p in find_zero_bit_positions(flip_arcs[j][1][0], n)) # 2b
-        for j in range(0,len(keep_arcs)):
+        for j in range(0, b):
             m.addConstr(x[keep_arcs[j], i] <= (1 - gp.quicksum(t[q, keep_arcs[j][0][1]] for q in range(1,N+1)) + gp.quicksum(w[q, keep_arcs[j][0][1]] for q in find_zero_bit_positions(keep_arcs[j][1][0], N)))) # 2c
     m.optimize()
     printSolution(m, n, d, t, w)
-    if (m.status == GRB.INFEASIBLE):
-        print("Model is infeasible.")
-        m.computeIIS()
-        print("IIS computed. Constraints that are conflicting:")
-        for c in m.getConstrs():
-            if c.IISConstr:
-                print(c.constrName)
-
 
 # Main
 setQubitsDict(0)
